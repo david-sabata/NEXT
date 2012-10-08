a610d6663ebccc29094c07f9b5428dae7f0a52a9
david-sabata <david.n.sabata@gmail.com>
2012-10-08 19:38:02 +0200

    fragments namisto views

--- NEXT/src/cz/fit/next/MainActivity.java
+++ NEXT/src/cz/fit/next/MainActivity.java
@@ -1,52 +1,82 @@
 package cz.fit.next;
 
-import com.deaux.fan.FanView;
-
+import android.annotation.TargetApi;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.ListFragment;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends Activity {
+import com.deaux.fan.FanView;
 
-	private FanView fan; 
-	
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        
-//        super.onCreate(savedInstanceState);
-        setContentView(R.layout.test);
-        
-        fan = (FanView) findViewById(R.id.fan_view);
-        fan.setViews(R.layout.main, R.layout.fan);
-        
-        /*
-         * fan.setFadeOnMenuToggle(true);
-         * fan.setAnimationDuration(500);
-         * fan.setIncludeDropshadow(false);
-         */ 
-    }
+public class MainActivity extends FragmentActivity {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
-    
-    public void unclick(View v) {
-    	System.out.println("CLOSE");
-    	fan.showMenu();
-    }
-    
-    public void click(View v) {
-    	System.out.println("OPEN");
-    	fan.showMenu();
-    } 
+	private FanView fan;
+
+
+	@TargetApi(14)
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.test);
+		fan = (FanView) findViewById(R.id.fan_view);
+
+		Fragment fanFrag = new SidebarFragment();
+		Fragment contentFrag = new ListFragment();
+		fan.setFragments(contentFrag, fanFrag);
+
+		// always enabled on SDK < 14
+		if (android.os.Build.VERSION.SDK_INT >= 14) {
+			getActionBar().setHomeButtonEnabled(true);
+		}
+	}
+
+
+	/**
+	 * Closes sidebar
+	 * 
+	 * @param v
+	 */
+	public void unclick(View v) {
+		System.out.println("CLOSE");
+		fan.showMenu();
+	}
+
+
+	/**
+	 * Opens sidebar
+	 * 
+	 * @param v
+	 */
+	public void click(View v) {
+		System.out.println("OPEN");
+		fan.showMenu();
+	}
+
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+
+	/**
+	 * On menu item clicked It can be either classic menu item or app icon in
+	 * action bar
+	 */
+	public boolean onOptionsItemSelected(MenuItem item) {
+		super.onOptionsItemSelected(item);
+
+		if (item.getItemId() == android.R.id.home) {
+			if (fan.isOpen()) {
+				unclick(null);
+			} else {
+				click(null);
+			}
+		}
+
+		return false;
+	}
+
 }
-
-
-
