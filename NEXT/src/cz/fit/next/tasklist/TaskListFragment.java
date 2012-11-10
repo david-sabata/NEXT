6705b057f1f7c3be8d939897fed67791a0317b2f
david-sabata <david.n.sabata@gmail.com>
2012-11-11 00:37:04 +0100

    task list context menu (deletion not implemented yet)

--- NEXT/src/cz/fit/next/tasklist/TaskListFragment.java
+++ NEXT/src/cz/fit/next/tasklist/TaskListFragment.java
@@ -5,16 +5,23 @@ import android.database.sqlite.SQLiteCursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
+import android.widget.Toast;
 import cz.fit.next.ContentReloadable;
 import cz.fit.next.MainActivity;
 import cz.fit.next.R;
 import cz.fit.next.backend.TasksModelService;
 import cz.fit.next.backend.database.Constants;
 import cz.fit.next.taskdetail.TaskDetailFragment;
+import cz.fit.next.taskdetail.TaskEditFragment;
 
 public class TaskListFragment extends ListFragment implements ContentReloadable {
 
@@ -43,6 +50,9 @@ public class TaskListFragment extends ListFragment implements ContentReloadable 
 		} catch (RuntimeException e) {
 			// ignore and wait for the next call
 		}
+
+		// register long click events
+		registerForContextMenu(getListView());
 	}
 
 
@@ -79,6 +89,48 @@ public class TaskListFragment extends ListFragment implements ContentReloadable 
 		// replace main fragment with task detail fragment
 		MainActivity activity = (MainActivity) getActivity();
 		activity.getFanView().replaceMainFragment(fTask);
+	}
+
+
+
+
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+
+		if (v.getId() == android.R.id.list) {
+			menu.add(Menu.NONE, R.id.action_edit, 0, R.string.action_edit_task);
+			menu.add(Menu.NONE, R.id.action_delete, 1, R.string.action_delete_task);
+		}
+		else {
+			Log.e(LOG_TAG, "fail");
+		}
+	}
+
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+
+		SQLiteCursor cursor = (SQLiteCursor) getListAdapter().getItem(info.position);
+		String taskId = cursor.getString(cursor.getColumnIndex(Constants.COLUMN_ID));
+
+		switch (item.getItemId()) {
+
+			case R.id.action_edit:
+				// switch to edit fragment (skipping the detail fragment)
+				TaskEditFragment fTask = TaskEditFragment.newInstance(taskId);
+				MainActivity activity = (MainActivity) getActivity();
+				activity.getFanView().replaceMainFragment(fTask);
+				break;
+
+			case R.id.action_delete:
+				Toast.makeText(getActivity(), "Task deletion not implemented yet", Toast.LENGTH_SHORT).show();
+				break;
+		}
+
+		return true;
 	}
 
 
