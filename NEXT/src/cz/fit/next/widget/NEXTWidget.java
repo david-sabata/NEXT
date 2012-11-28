310a712396b77edf90550fdf4658a4c14cad2fa9
Martin Hala <hali.m@email.cz>
2012-11-28 02:08:00 +0100

    Widget with button, which on click create new task

--- NEXT/src/cz/fit/next/widget/NEXTWidget.java
+++ NEXT/src/cz/fit/next/widget/NEXTWidget.java
@@ -1,7 +1,31 @@
 package cz.fit.next.widget;
 
+import cz.fit.next.R;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
 
 public class NEXTWidget extends AppWidgetProvider {
+	
+	public static String ACTION_WIDGET_CONFIGURE = "ConfigureWidget";
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		
+		RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+		Intent configIntent = new Intent(context, ClickAddActivity.class);
+		configIntent.setAction(ACTION_WIDGET_CONFIGURE);
+	
+		//active.putExtra("msg", "Message for Button 1");
+		PendingIntent actionPendingIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
+		//PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
+		remoteViews.setOnClickPendingIntent(R.id.button_add, actionPendingIntent);
+		//remoteViews.setOnClickPendingIntent(R.id.button_two, configPendingIntent);
+		appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+	}
+
 
 }
