6b2c210abfb4aa66f7b4a117580fb436857ee9ef
Tomas Sychra <xsychr03@gmail.com>
2013-01-15 20:49:17 +0100

    zakl. vlastnosti prepsany do stylu; otazka je co vsechno prepisovat?

--- NEXT/src/cz/fit/next/SettingsFragment.java
+++ NEXT/src/cz/fit/next/SettingsFragment.java
@@ -1,11 +1,7 @@
 package cz.fit.next;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class SettingsFragment extends PreferenceFragment {
 
