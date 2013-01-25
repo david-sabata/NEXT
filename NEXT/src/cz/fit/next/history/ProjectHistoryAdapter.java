1666c7706d704af1f8f50a196b353af23f773775
Tomsa <xholom01@stud.fit.vutbr.cz>
2013-01-25 14:24:44 +0100

    ProjectHistoryAdapter - records rendering

--- NEXT/src/cz/fit/next/history/ProjectHistoryAdapter.java
+++ NEXT/src/cz/fit/next/history/ProjectHistoryAdapter.java
@@ -7,6 +7,7 @@ import cz.fit.next.backend.TaskHistory;
 import cz.fit.next.backend.TasksModelService;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import android.content.Context;
 import android.database.Cursor;
@@ -14,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,6 +23,8 @@ public class ProjectHistoryAdapter extends ArrayAdapter<TaskHistory> {
 
 	ArrayList<TaskHistory> mData;
 	Context mContext;
+	
+	private HashMap<String, String> fieldnames;
 
 	public ProjectHistoryAdapter(Context context, int textViewResourceId,
 			ArrayList<TaskHistory> history) {
@@ -28,6 +32,20 @@ public class ProjectHistoryAdapter extends ArrayAdapter<TaskHistory> {
 
 		mData = history;
 		mContext = context;
+
+		// fill in field names translator
+		fieldnames = new HashMap<String, String>();
+
+		fieldnames.put(TaskHistory.TITLE, getContext().getResources()
+				.getString(R.string.history_title));
+		fieldnames.put(TaskHistory.CONTEXT, getContext().getResources()
+				.getString(R.string.history_context));
+		fieldnames.put(TaskHistory.DATE,
+				getContext().getResources().getString(R.string.history_date));
+		fieldnames.put(TaskHistory.DESCRIPTION, getContext().getResources()
+				.getString(R.string.history_description));
+		fieldnames.put(TaskHistory.PRIORITY, getContext().getResources()
+				.getString(R.string.history_priority));
 	}
 
 	@Override
@@ -44,14 +62,13 @@ public class ProjectHistoryAdapter extends ArrayAdapter<TaskHistory> {
 		TextView subtitle = (TextView) vi.findViewById(R.id.subtitle);
 
 		// Title --> name of task
-		Task t = TasksModelService.getInstance()
-				.getTaskById(mData.get(position).getTaskId());
+		Task t = TasksModelService.getInstance().getTaskById(
+				mData.get(position).getTaskId());
 		if (t != null) {
 			title.setText(t.getTitle());
 		} else {
-			title.setText("*DELETED*");
+			title.setText("*DELETED TASK*");
 		}
-		
 
 		// Author + timestamp
 		String sub = "";
@@ -64,14 +81,66 @@ public class ProjectHistoryAdapter extends ArrayAdapter<TaskHistory> {
 		author.setText(sub);
 
 		// Changes in tasks
+		// Image
+		ImageView img = (ImageView) vi.findViewById(R.id.history_image);
+
 		sub = "";
 
 		for (int i = 0; i < mData.get(position).getChanges().size(); i++) {
-			sub = sub + mData.get(position).getChanges().get(i).getName() + ":"
-					+ mData.get(position).getChanges().get(i).getOldValue()
-					+ " -> "
-					+ mData.get(position).getChanges().get(i).getNewValue()
-					+ "\n";
+
+			if ((mData.get(position).getChanges().get(i).getName()
+					.equals(TaskHistory.TITLE))
+					&& (mData.get(position).getChanges().get(i).getOldValue()
+							.isEmpty())) {
+				sub = sub + "TASK CREATED" + "\n";
+
+				img.setImageResource(R.drawable.action_add);
+
+				break;
+			}
+
+			if ((mData.get(position).getChanges().get(i).getName()
+					.equals(TaskHistory.COMPLETED))
+					&& (mData.get(position).getChanges().get(i).getNewValue()
+							.equals("true"))) {
+				sub = sub + "TASK MARKED AS COMPLETE" + "\n";
+
+				img.setImageResource(R.drawable.action_accept);
+			}
+
+			if ((mData.get(position).getChanges().get(i).getName()
+					.equals(TaskHistory.COMPLETED))
+					&& (mData.get(position).getChanges().get(i).getNewValue()
+							.equals("false"))) {
+				sub = sub + "TASK MARKED AS UNCOMPLETE" + "\n";
+
+				img.setImageResource(R.drawable.action_cancel);
+			}
+
+			if (mData.get(position).getChanges().get(i).getName()
+					.equals(TaskHistory.DATE)) {
+				sub = sub
+						+ fieldnames.get(mData.get(position).getChanges()
+								.get(i).getName())
+						+ " -> "
+						+ new DateTime(Long.parseLong(mData.get(position)
+								.getChanges().get(i).getNewValue()))
+								.toLocaleDateTimeString() + "\n";
+			}
+
+			if ((mData.get(position).getChanges().get(i).getName()
+					.equals(TaskHistory.CONTEXT))
+					|| (mData.get(position).getChanges().get(i).getName()
+							.equals(TaskHistory.PRIORITY))
+					|| (mData.get(position).getChanges().get(i).getName()
+							.equals(TaskHistory.TITLE))) {
+
+				sub = sub
+						+ fieldnames.get(mData.get(position).getChanges()
+								.get(i).getName()) + " -> "
+						+ mData.get(position).getChanges().get(i).getNewValue()
+						+ "\n";
+			}
 		}
 
 		subtitle.setText(sub);
