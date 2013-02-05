7d4d31e94d867937bf21ccae307d7b5040cead7f
david-sabata <david.n.sabata@gmail.com>
2013-02-05 14:16:23 +0100

    click on item in In plan list shows task detail

--- NEXT/src/cz/fit/next/inplan/InplanFragment.java
+++ NEXT/src/cz/fit/next/inplan/InplanFragment.java
@@ -2,11 +2,16 @@ package cz.fit.next.inplan;
 
 import android.app.ListFragment;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteCursor;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
 import cz.fit.next.MainActivity;
 import cz.fit.next.R;
 import cz.fit.next.ServiceReadyListener;
 import cz.fit.next.backend.TasksModelService;
+import cz.fit.next.backend.database.Constants;
+import cz.fit.next.taskdetail.TaskDetailFragment;
 
 
 
@@ -71,5 +76,24 @@ public class InplanFragment extends ListFragment implements ServiceReadyListener
 		if (getActivity() != null)
 			reload();
 	}
+
+
+
+
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		// get task id
+		SQLiteCursor cursor = (SQLiteCursor) getListAdapter().getItem(position);
+		String taskId = cursor.getString(cursor.getColumnIndex(Constants.COLUMN_ID));
+
+		// crate fragment with task detail
+		TaskDetailFragment fTask = TaskDetailFragment.newInstance(taskId);
+
+		// replace main fragment with task detail fragment
+		MainActivity activity = (MainActivity) getActivity();
+		activity.replaceMainFragment(fTask);
+	}
+
 
 }
