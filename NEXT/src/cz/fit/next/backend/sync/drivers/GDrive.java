847c287cc4f77f3d0695b356001be29328624b4b
Tomsa <xholom01@stud.fit.vutbr.cz>
2013-02-05 10:36:36 +0100

    GDrive - added file filtering

--- NEXT/src/cz/fit/next/backend/sync/drivers/GDrive.java
+++ NEXT/src/cz/fit/next/backend/sync/drivers/GDrive.java
@@ -96,6 +96,15 @@ public class GDrive {
 		//mCallback = cb;
 		
 		List<File> res = getFileList(mAppFolder);
+		
+		for (int i = 0; i < res.size(); i++) {
+			if (!res.get(i).getTitle().contains(".nextproj.html")) {
+				Log.i(TAG,"REMOVED " + res.get(i).getTitle());
+				res.remove(i);
+				i--;
+			}
+		}
+		
 		return res;		
 		
 	}
