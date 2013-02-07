ddf6dd1761a3e0d257e2249414df2e09fead862d
Tomsa <xholom01@stud.fit.vutbr.cz>
2013-02-07 19:55:14 +0100

    NotificationService - notifying

--- NEXT/src/cz/fit/next/notifications/NotificationService.java
+++ NEXT/src/cz/fit/next/notifications/NotificationService.java
@@ -1,15 +1,18 @@
 package cz.fit.next.notifications;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
+import cz.fit.next.R;
 import cz.fit.next.backend.DateTime;
 import cz.fit.next.backend.SettingsProvider;
 import cz.fit.next.backend.Task;
 import cz.fit.next.backend.database.TasksDataSource;
-import cz.fit.next.backend.sync.SyncService;
-import cz.fit.next.backend.sync.SyncService.ServiceBinder;
-import cz.fit.next.preferences.SettingsFragment;
+import cz.fit.next.notifications.NotificationsAlarmReceiver;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Binder;
@@ -17,9 +20,14 @@ import android.os.IBinder;
 import android.util.Log;
 
 public class NotificationService extends Service {
-	
+
 	private final String TAG = "NEXT Notification Service";
-	
+
+	private class Params {
+		public Task upcoming;
+		public ArrayList<Task> notifications;
+	};
+
 	public class ServiceBinder extends Binder {
 		public NotificationService getService() {
 			return NotificationService.this;
@@ -31,7 +39,7 @@ public class NotificationService extends Service {
 	@Override
 	public IBinder onBind(Intent arg0) {
 		SettingsProvider sp = new SettingsProvider(getApplicationContext());
-		
+
 		return mBinder;
 	}
 
@@ -42,45 +50,98 @@ public class NotificationService extends Service {
 
 		SettingsProvider sp = new SettingsProvider(getApplicationContext());
 		
+		Params p = searchForUpcomingTask();
+		setAlarm(p.upcoming.getDate().toMiliseconds());
 		
+		for (int i = 0; i < p.notifications.size(); i++) {
+			showNotification(p.notifications.get(i));
+		}
+		
+		stopSelf();
+
 		return START_NOT_STICKY;
 	}
-	
-	
-	private Task searchForUpcomingTask() {
+
+	private Params searchForUpcomingTask() {
+		
+		Params p = new Params();
+		p.notifications = new ArrayList<Task>();
+		
 		TasksDataSource ds = new TasksDataSource(getApplicationContext());
 		ds.open();
-		
+
 		Cursor c = ds.getAllTasksCursor();
 		c.moveToFirst();
 
 		Task t = new Task(c);
 		DateTime upcomingTime = t.getDate();
 		String upcomingId = t.getId();
-		
+
 		DateTime current = new DateTime();
-		
+
 		while (c.moveToNext()) {
 			t = new Task(c);
-			
-			if (t.getDate().isSomeday()) continue;
-			if (t.getDate().isAllday()) continue; // TODO: ????
-			if (t.getDate().toMiliseconds() < current.toMiliseconds()) continue;
-			if (t.getDate().toMiliseconds() > upcomingTime.toMiliseconds()) continue;
-						
+
+			if (t.getDate().isSomeday())
+				continue;
+			if (t.getDate().isAllday())
+				continue; // TODO: ????
+			if (t.getDate().toMiliseconds() < current.toMiliseconds())
+				continue;
+			if (t.getDate().toMiliseconds() > upcomingTime.toMiliseconds())
+				continue;
+
+			if ((t.getDate().toCalendar().get(Calendar.HOUR)) == (current
+					.toCalendar().get(Calendar.HOUR))
+					&& ((t.getDate().toCalendar().get(Calendar.MINUTE)) == (current
+							.toCalendar().get(Calendar.MINUTE)))) {
+				
+				p.notifications.add(t);
+			}
+
 			upcomingTime = t.getDate();
 			upcomingId = t.getId();
 		}
-		
-		Task ret = ds.getTaskById(upcomingId);
+
+		p.upcoming = ds.getTaskById(upcomingId);
 		ds.close();
-		
-		Log.i(TAG,"Selected upcoming task: " + ret.getTitle());
-		
-		return ret;
-		
+
+		Log.i(TAG, "Selected upcoming task: " + p.upcoming.getTitle());
+
+		return p;
+
+	}
+
+	private void setAlarm(long time) {
+
+		NotificationsAlarmReceiver ar = new NotificationsAlarmReceiver(
+				getApplicationContext(), time);
+
 	}
 	
-	
+	private void showNotification(Task t) {
+		
+		String title = getResources().getString(R.string.upcoming_notification);
+		String content = t.getTitle();
+		String ticker = getResources().getString(R.string.upcoming_notification);
+		
+		Notification.Builder mBuilder = new Notification.Builder(this).setSmallIcon(R.drawable.menu_next)
+				.setContentTitle(title).setContentText(content)
+				.setTicker(ticker).setAutoCancel(true);
+
+		//		Intent resultIntent = new Intent(this, MainActivity.class);
+		
+		//		TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+		//		stackBuilder.addParentStack(MainActivity.class);
+		//		stackBuilder.addNextIntent(resultIntent);
+		//		PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+		//		mBuilder.setContentIntent(resultPendingIntent);
+
+
+		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+		int notid = 0;
+		mNotificationManager.notify(notid, mBuilder.getNotification());
+	}
 
 }
