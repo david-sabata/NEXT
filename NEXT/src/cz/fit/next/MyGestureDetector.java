9a61677c9cf6b941f94019a6521f21f6886f2bc7
david-sabata <david.n.sabata@gmail.com>
2013-02-11 10:14:49 +0100

    fix sidebar gesture detection, closes #46

--- NEXT/src/cz/fit/next/MyGestureDetector.java
+++ NEXT/src/cz/fit/next/MyGestureDetector.java
@@ -7,8 +7,8 @@ import com.deaux.fan.FanView;
 
 public class MyGestureDetector extends SimpleOnGestureListener {
 
-	private static final int SWIPE_MIN_DISTANCE = 120;
-	private static final int SWIPE_MAX_OFF_PATH = 250;
+	private static final int SWIPE_MIN_DISTANCE = 150;
+	private static final int SWIPE_MAX_OFF_PATH = 150;
 	private static final int SWIPE_THRESHOLD_VELOCITY = 100;
 
 
@@ -23,16 +23,17 @@ public class MyGestureDetector extends SimpleOnGestureListener {
 	@Override
 	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
 		try {
-			//			Log.d("FROM", e1.getX() + " | " + e1.getY());
-			//			Log.d("TO", e2.getX() + " | " + e2.getY());
 
 			if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
 				return false;
+
 			// right to left swipe
 			if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
 				if (mFanView.isOpen()) {
 					mFanView.showMenu();
 				}
+
+				// left to right swipe
 			} else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
 				if (!mFanView.isOpen()) {
 					mFanView.showMenu();
