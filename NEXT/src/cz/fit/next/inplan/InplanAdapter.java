19204b61b91af45198d4b79a509663763263e51b
david-sabata <david.n.sabata@gmail.com>
2013-02-13 13:38:44 +0100

    use list_inplan_nosubtitle layout for allday tasks at Inplan screen

--- NEXT/src/cz/fit/next/inplan/InplanAdapter.java
+++ NEXT/src/cz/fit/next/inplan/InplanAdapter.java
@@ -75,7 +75,7 @@ public class InplanAdapter extends CursorAdapter implements ListAdapter {
 			secHeader.setVisibility(View.VISIBLE);
 			TextView text = (TextView) secHeader;
 			text.setText(currDatetime.toLocaleDateString());
-			
+
 			// Separator line
 			secHeaderLine.setVisibility(View.VISIBLE);
 		} else {
@@ -129,18 +129,16 @@ public class InplanAdapter extends CursorAdapter implements ListAdapter {
 		TextView dt = (TextView) view.findViewById(R.id.subtitle);
 		long date = cursor.getLong(cursor.getColumnIndex(Constants.COLUMN_DATETIME));
 		DateTime datetime = new DateTime(date);
-				
+
 		if (!datetime.isAllday()) {
 			dt.setText(datetime.toLocaleTimeString());
 		}
-			
-		
 
 
 		// priority
 		LinearLayout prl = (LinearLayout) view.findViewById(R.id.TasklistItemPriority);
 		if (cursor.getColumnIndex(Constants.COLUMN_PRIORITY) != -1) {
-			Integer priority = Integer.parseInt(cursor.getString(cursor.getColumnIndex(Constants.COLUMN_PRIORITY)));
+			int priority = cursor.getInt(cursor.getColumnIndex(Constants.COLUMN_PRIORITY));
 			switch (priority) {
 				case 1:
 					prl.setBackgroundColor(context.getResources().getColor(R.color.priority_1));
@@ -161,8 +159,14 @@ public class InplanAdapter extends CursorAdapter implements ListAdapter {
 	@Override
 	public View newView(Context context, Cursor cursor, ViewGroup parent) {
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				
-		return inflater.inflate(R.layout.task_list_inplan_item, null, true);
+
+		long date = cursor.getLong(cursor.getColumnIndex(Constants.COLUMN_DATETIME));
+		DateTime datetime = new DateTime(date);
+
+		if (datetime.isAllday())
+			return inflater.inflate(R.layout.task_list_inplan_item_nosubtitle, null, true);
+		else
+			return inflater.inflate(R.layout.task_list_inplan_item, null, true);
 	}
 
 
